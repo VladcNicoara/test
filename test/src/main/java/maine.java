4712ab4cfd211deefceae50dca16a6846ee68bfc
Vlad Nicoara <vlad.nicoara@elan-solutions.eu>
2015-09-28 14:25:45 +0300

    Test commit (ignore)

---
+++ test/src/main/java/maine.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Vlad Nicoara on 28.09.2015.
+ */
+public class maine {
+public static void main(String[] args)
+{
+    System.out.print("Hello V");
+    System.out.print("Hello 2");
+    System.out.print("Hello 2");
+}
+}
---
+++ test/src/main/java/thirdtest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Vlad Nicoara on 28.09.2015.
+ */
+public class thirdtest {
+}
